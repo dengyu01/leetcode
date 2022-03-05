000379128f1d216c0f7b59d4e838f3d9831c2a04
hscovo <hscovo@mail.ustc.edu.cn>
2022-03-05 15:39:29 +0800

    Solve leetcode 521

---
+++ src/com/hsc/string/Solution521.java
@@ -0,0 +1,7 @@
+package com.hsc.string;
+
+public class Solution521 {
+    public int findLUSlength(String a, String b) {
+        return !a.equals(b) ? Math.max(a.length(), b.length()) : -1;
+    }
+}
